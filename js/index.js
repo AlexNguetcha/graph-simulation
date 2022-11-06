1572ac78993c94d144db8e89b49f4e2f6f062d9d
alexnguetcha <nguetchaalex@gmail.com>
2022-11-06 02:43:48 +0100

    add vertex

--- js/index.js
+++ js/index.js
@@ -2,20 +2,22 @@
 const ACTIONS = [
     {
         id: 'ADD',
-        name: 'Ajouter un sommet'
+        name: 'Add a node'
     },
     {
         id: 'LINK',
-        name: 'Lier des sommets'
+        name: 'Link nodes'
     },
     {
         id: 'DELETE',
-        name: 'Supprimer un objet'
+        name: 'Delete object'
     }
 ];
 var graph = [];
 var nodes = [];
+var vertex = [];
 var currentAction = ACTIONS[0];
+var nodeToLink = null;
 
 const initActions = (actions, actionsNode) => {
     actions.forEach((action, key) => {
@@ -49,6 +51,81 @@ const getHigherNodeId = () => {
     return lastHightNodeId + 1
 }
 
+const removeNode = (node) => {
+    nodes = nodes.filter((value, index) => {
+        return parseInt(value.dataset.nodeId) != parseInt(node.dataset.nodeId)
+    })
+    vertex = vertex.filter((value, index)=>{
+        if(value.from==node || value.to==node){
+            removeVertex(document.getElementById(value.id))
+        }
+        return !(value.from==node || value.to==node)
+    })
+    node.remove()
+    updateGraph()
+}
+
+const removeVertex = (node) => {
+    vertex = vertex.filter(({ from, to, weight, id }, index) => {
+        return node.getAttribute('id') != id
+    })
+    node.remove()
+    updateGraph()
+}
+
+const drawVertex = (container, id, [x1, y1], [x2, y2], weight, color = '#333', thickness = 3) => {
+    let length = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+    let cx = ((x1 + x2) / 2) - (length / 2);
+    let cy = ((y1 + y2) / 2) - (thickness / 2);
+    let angle = Math.atan2((y1 - y2), (x1 - x2)) * (180 / Math.PI);
+
+    var lineNode = document.createElement('div')
+    lineNode.id = id
+    lineNode.classList = 'vertex'
+    lineNode.style.height = thickness + "px";
+    lineNode.style.backgroundColor = color;
+    lineNode.style.left = cx + "px";
+    lineNode.style.top = cy + "px";
+    lineNode.style.width = length + "px";
+    lineNode.style.transform = `rotate(${angle}deg)`;
+    lineNode.style.backgroundColor = color;
+
+    let lineNodeText = document.createElement('div')
+    lineNodeText.style.backgroundColor = color;
+    lineNodeText.style.transform = `rotate(-${angle * -1}deg)`;
+    lineNodeText.style.backgroundColor = color;
+    lineNodeText.innerText = weight
+
+    lineNode.append(lineNodeText)
+    lineNode.addEventListener('click', () => {
+        if (currentAction == ACTIONS[2]) {
+            removeVertex(lineNode)
+        }
+    })
+    container.append(lineNode)
+    updateGraph()
+}
+
+const graphHasVertex = (node1, node2) => {
+    let has = false
+    vertex.forEach(({ from, to, weight, id }) => {
+        if (from == node1 && to == node2) {
+            has = true
+        }
+    });
+    return has;
+}
+
+const findVertex = (node1, node2)=>{
+    let v = null
+    vertex.forEach(({ from, to, weight, id }) => {
+        if ((from == node1 && to == node2) || (from == node2 && to == node1)) {
+            v = { from, to, weight, id }
+        }
+    });
+    return v;
+}
+
 const addNode = (x, y, nodeContainer) => {
     let node = document.createElement('div')
     node.classList = 'graph-node'
@@ -57,9 +134,56 @@ const addNode = (x, y, nodeContainer) => {
     node.style.top = y + 'px'
     node.setAttribute('data-node-id', getHigherNodeId())
     node.innerHTML = node.getAttribute('data-node-id');
+    node.addEventListener('click', () => {
+        // console.log(`click on node ${node.innerHTML}`);
+        if (currentAction == ACTIONS[2]) {
+            removeNode(node)
+        } else if (currentAction == ACTIONS[1]) {
+            if (nodeToLink != null) {
+                nodeToLink.style.background = 'var(--green)'
+                if (!graphHasVertex(nodeToLink, node)) {
+                    // create a vertex
+                    let vertexId = (new Date()).getTime()
+                    vertex.push({
+                        from: nodeToLink,
+                        to: node,
+                        weight: 2,
+                        id: vertexId
+                    })
+
+                    drawVertex(
+                        document.querySelector('#graph-container'),
+                        vertexId,
+                        [nodeToLink.style.left, nodeToLink.style.top].map(e => e.replace('px', '')).map(Number).map(e => (e + 30)),
+                        [node.style.left, node.style.top].map(e => e.replace('px', '')).map(Number).map(e => (e + 30)),
+                        2
+                    )
+                }
+                nodeToLink = null
+            } else {
+                node.style.background = 'red';
+                nodeToLink = node
+            }
+        }
+    })
     nodes.push(node)
-    console.log(nodes);
     nodeContainer.append(node)
+    updateGraph()
+}
+
+const updateGraph = () => {
+    let newGraph = []
+    nodes.forEach(element => {
+        let elementRelations = Array(nodes.length).fill(0)
+        nodes.forEach((elt, key) => {
+            if(graphHasVertex(element, elt) || graphHasVertex(elt, element)){
+                elementRelations[key] = findVertex(element, elt).weight
+            }
+        });
+        newGraph.push(elementRelations)
+    });
+    graph = newGraph
+    console.log(graph);
 }
 
 const initGraphEventListener = (graphNode) => {
@@ -67,8 +191,7 @@ const initGraphEventListener = (graphNode) => {
         const [add, link, remove] = ACTIONS
         switch (currentAction) {
             case add:
-
-                addNode(e.clientX-30, e.clientY-30, graphNode)
+                addNode(e.clientX - 30, e.clientY - 30, graphNode)
                 break;
             case link:
 
